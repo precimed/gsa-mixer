cc34ea77f7ee53e2e3948d59fc76e2696af74c7c
Oleksandr Frei <oleksandr.frei@gmail.com>
2024-05-21 09:22:28 +0200

    update versions in apt_get_essential.sh

--- scripts/apt_get_essential.sh
+++ scripts/apt_get_essential.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-apt-get update && apt-get install -y --no-install-recommends apt-utils=2.0.9
-apt-get update && apt-get install -y --no-install-recommends ca-certificates=20211016~20.04.1 && \
+set -euo pipefail
+
+apt-get update && apt-get install -y --no-install-recommends apt-utils=2.0.10
+apt-get update && apt-get install -y --no-install-recommends ca-certificates=20230311ubuntu0.20.04.1 && \
    update-ca-certificates
 
 # (!) Keep the list below sorted (!)
@@ -10,30 +12,35 @@ apt-get update && apt-get install -y --no-install-recommends \
    build-essential=12.8ubuntu1 \
    bzip2=1.0.8-2 \
    cmake=3.16.3-1ubuntu1.20.04.1 \
-   curl=7.68.0-1ubuntu2.14 \
+   curl=7.68.0-1ubuntu2.21 \
    dos2unix=7.4.0-2 \
+   gdb=9.1-0ubuntu1 \
    gfortran=4:9.3.0-1ubuntu2 \
-   git=1:2.25.1-1ubuntu3.6 \
+   git=1:2.25.1-1ubuntu3.11 \
    less=551-1ubuntu0.1 \
    libatlas-base-dev=3.10.3-8ubuntu7 \
-   libcurl4-openssl-dev=7.68.0-1ubuntu2.14 \
-   libgomp1=10.3.0-1ubuntu1~20.04 \
-   libgsl-dev=2.5+dfsg-6build1 \
-   libnss3=2:3.49.1-1ubuntu1.8 \
+   libcurl4-openssl-dev=7.68.0-1ubuntu2.21 \
+   libgomp1=10.5.0-1ubuntu1~20.04 \
+   libgsl-dev=2.5+dfsg-6+deb10u1build0.20.04.1 \
+   libnss3=2:3.49.1-1ubuntu1.9 \
    libpcre2-dev=10.34-7ubuntu0.1 \
    libxt-dev=1:1.1.5-1 \
    pandoc=2.5-3build2 \
    pandoc-citeproc=0.15.0.1-1build4 \
    parallel=20161222-1.1 \
-   perl=5.30.0-9ubuntu0.3 \
+   perl=5.30.0-9ubuntu0.5 \
    pkg-config=0.29.1-0ubuntu4 \
-   tar=1.30+dfsg-7ubuntu0.20.04.2 \
+   python3=3.8.2-0ubuntu2 \
+   tar=1.30+dfsg-7ubuntu0.20.04.4 \
    tofrodos=1.7.13+ds-4 \
    unzip=6.0-25ubuntu1.1 \
-   vim=2:8.1.2269-1ubuntu5.9 \
+   vim=2:8.1.2269-1ubuntu5.21 \
    wget=1.20.3-1ubuntu2 \
    zlib1g-dev=1:1.2.11.dfsg-2ubuntu1.5 \
    && \
    apt-get clean && \
    rm -rf /var/lib/apt/lists/*
    
+# /usr/bin/python must exist for bgenix, qctool
+update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+
