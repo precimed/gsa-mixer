6b7cb34e99f64fc4f372cb5b0cce987f08ead945
Oleksandr Frei <oleksandr.frei@gmail.com>
2025-09-05 22:26:09 +0200

    Update docker files

--- docker/scripts/install_plink2.sh
+++ docker/scripts/install_plink2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euo pipefail
 
-version=20250825
+version=20250901
 wget --no-check-certificate https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_$version.zip && \
     unzip -j plink2_linux_x86_64_$version.zip && \
     rm -rf plink2_linux_x86_64_$version.zip
