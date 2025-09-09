9c40fd1482a601ae6473fff9bfcea2d82d17ff5a
Oleksandr Frei <oleksandr.frei@gmail.com>
2025-09-09 15:41:44 +0200

    update precimed/version.py to dev-2025-09-09

--- precimed/version.py
+++ precimed/version.py
@@ -6,7 +6,7 @@ _PATCH = "0"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
 #_SUFFIX = ""
-_SUFFIX = "dev-2025-09-08"
+_SUFFIX = "dev-2025-09-09"
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}{3}".format(_MAJOR, _MINOR, _PATCH, _SUFFIX)
