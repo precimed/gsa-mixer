f534e7fbb1da56739f6592a326ae17ee89cffb31
Oleksandr Frei <oleksandr.frei@gmail.com>
2026-09-18 13:37:09 +0200

    Fix PLINK 2 download and update pinned build to 20260914

--- docker/scripts/install_plink2.sh
+++ docker/scripts/install_plink2.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -euo pipefail
 
-version=20250901
-wget --no-check-certificate https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_$version.zip && \
-    unzip -j plink2_linux_x86_64_$version.zip && \
-    rm -rf plink2_linux_x86_64_$version.zip
+version=20260914
+archive="plink2_linux_x86_64_${version}.zip"
+# Pinned Linux 64-bit build from https://www.cog-genomics.org/plink/2.0/
+wget "https://s3.amazonaws.com/plink2-assets/alpha7/${archive}"
+unzip -j "$archive"
+rm "$archive"
 
 cp plink2 /bin
+plink2 --version
